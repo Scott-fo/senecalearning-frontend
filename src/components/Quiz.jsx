cbd607146f8d5b66cd5199a6b1776581c1e430e5
Scott-fo <scottfo@live.com>
2022-04-21 18:14:08 +0100

    dynamic colour

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,14 +5,28 @@ import Answers from './Answers';
 
 const Quiz = () => {
   const [correct, setCorrect] = useState(false);
-  const [prevName, setPrevName] = useState({});
+  const [selectedList, setSelectedList] = useState({});
   const correctList = Questions[0].correctAnswers;
 
   const handleClick = (e) => {
     const name = e.currentTarget.name;
     const target = e.currentTarget.id;
-    setPrevName({...prevName, [name] : target });
+    setSelectedList({...selectedList, [name] : target });
   }
+
+  const updateColours = () => {
+    if (correct) {
+      document.documentElement.style.setProperty("--background-gradient", "linear-gradient(180deg, #76E0C2 0%, #59CADA 100%)");
+      document.documentElement.style.setProperty("--answer-background", "#A5E7E2");
+      document.documentElement.style.setProperty("--answer-border", "#A5E7E2");
+      document.documentElement.style.setProperty("--answer-text", "#4CAD94");
+    } else {
+      document.documentElement.style.setProperty("--background-gradient", "linear-gradient(180deg, #F6B868 0%, #EE6B2D 100%)");
+      document.documentElement.style.setProperty("--answer-background", "#F9D29F");
+      document.documentElement.style.setProperty("--answer-border", "#F9D29F");
+      document.documentElement.style.setProperty("--answer-text", "#9F938B");
+    }
+  };
 
   const answerComponent = Questions[0].answers.map((answer, key) => {
     return (
@@ -20,26 +34,34 @@ const Quiz = () => {
         key={key}
         id={key}
         {...answer}
+        correct={correct}
         handleClick={(e) => handleClick(e)}
       />
     )
   });
 
   useEffect(() => {
-    if (JSON.stringify(correctList) === JSON.stringify(Object.values(prevName))) {
+    if (JSON.stringify(correctList) === JSON.stringify(Object.values(selectedList))) {
       setCorrect(true);
+      updateColours(correct);
+    } else if ((correctList.filter(item => Object.values(selectedList).includes(item))).length >= 2) {
+        setCorrect(false);
+        document.documentElement.style.setProperty("--background-gradient", "linear-gradient(180deg, #F1B496 0%, #EA806A 100%)");
+        document.documentElement.style.setProperty("--answer-background", "#F2CBBD");
+        document.documentElement.style.setProperty("--answer-border", "#F2CBBD");
+        document.documentElement.style.setProperty("--answer-text", "#E47958");
     } else {
-      setCorrect(false);
+        setCorrect(false);
+        updateColours(correct);
     }
-    console.log("Use Effect")
-  }, [prevName, correctList])
+  }, [selectedList, correctList, correct])
 
   return (
     <div className='quiz-wrapper'>
       <div className="quiz-container">
         <h1 className='question'>{Questions[0].question}</h1>
         {answerComponent}
-        <h2 className='result'>The answer is {correct ? "correct" : "incorrect"}</h2>
+        <h2 className='result'>The answer is {correct ? "correct!" : "incorrect"}</h2>
       </div>
     </div>
   )
