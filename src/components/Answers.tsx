e2f004225c0f3de643c23fc85430adc075349376
Scott-fo <scottfo@live.com>
2022-04-23 14:22:12 +0100

    changed to tsx

---
+++ src/components/Answers.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+
+interface Props {
+  shuffle: (array: any[]) => any[];
+  handleClick: React.MouseEventHandler<HTMLInputElement>;
+  answer: string[];
+  id: number;
+  correct: boolean;
+}
+
+const Answers = ({ shuffle, answer, id, handleClick, correct }: Props) => {
+  const [shuffledArray, setShuffledArray] = useState<any[]>([]);
+
+  useEffect(() => {
+    setShuffledArray(shuffle(answer));
+    // eslint-disable-next-line
+  }, [answer]);
+  
+  return (
+    <div className='answers'>
+      <section className="answer-section">
+        <input className="answer-input" type="radio" value={shuffledArray[0]} id={shuffledArray[0]} name={String(id)} onClick={handleClick} disabled={correct ? true : false}/>
+        <label className='answer' htmlFor={shuffledArray[0]}>{shuffledArray[0]}</label>
+      </section>
+      <section className="answer-section">
+        <input className="answer-input" type="radio" value={shuffledArray[1]} id={shuffledArray[1]} name={String(id)} onClick={handleClick} disabled={correct ? true : false}/>
+        <label className='answer' htmlFor={shuffledArray[1]}>{shuffledArray[1]}</label>
+      </section>
+    </div>
+  )
+}
+
+export { Answers };
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,35 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import "../styles/Quiz.css";
-import Questions from "../questions"
-import Answers from './Answers';
+import Questions from "../questions.json";
+import { Answers } from './Answers';
 
 const Quiz = () => {
   const [correct, setCorrect] = useState(false);
   const [selectedList, setSelectedList] = useState({});
-  const [shuffledAnswers, setShuffledAnswers] = useState([]);
+  const [shuffledAnswers, setShuffledAnswers] = useState<any[]>([]);
   const correctList = Questions[0].correctAnswers;
+  const answerList = Questions[0].answers;
 
-  useEffect(() => {
-    setShuffledAnswers(shuffle(Questions[0].answers))
-  }, [Questions[0].answers]);
-
-  useEffect(() => {
-    checkCorrectness();
-  }, [selectedList, correctList, correct]);
-
-  const handleClick = (e) => {
+  
+  
+  const handleClick = (e: any): void => {
     const name = e.currentTarget.name;
     const target = e.currentTarget.id;
     setSelectedList({...selectedList, [name] : target });
   }
-
-  const updateColours = (gradient, answerBackground, answerText) => {
+  
+  const updateColours = (gradient: string, answerBackground: string, answerText: string) => {
     document.documentElement.style.setProperty("--background-gradient", gradient);
     document.documentElement.style.setProperty("--answer-background", answerBackground);
     document.documentElement.style.setProperty("--answer-text", answerText);
   }
-
-  const shuffle = (array) => {
+  
+  const shuffle = useCallback((array: any[]): any[] => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       const temp = array[i];
@@ -37,29 +32,38 @@ const Quiz = () => {
       array[j] = temp;
     }
     return array;
-  }
-
-  const checkCorrectness = () => {
+  }, []);
+  
+  useEffect(() => {
+    const shuffledAnswers = shuffle(answerList)
+    setShuffledAnswers(shuffledAnswers)
+  }, [shuffle, answerList]);
+  
+  const checkCorrectness = useCallback(() => {
     if (JSON.stringify(correctList.sort()) === JSON.stringify(Object.values(selectedList).sort())) {
       setCorrect(true);
       updateColours("linear-gradient(180deg, #76E0C2 0%, #59CADA 100%)", "#A5E7E2", "#4CAD94");
     } else if ((correctList.filter(item => Object.values(selectedList).includes(item))).length >= 2) {
-        setCorrect(false);
-        updateColours("linear-gradient(180deg, #F1B496 0%, #EA806A 100%)", "#F2CBBD", "#E47958");
+      setCorrect(false);
+      updateColours("linear-gradient(180deg, #F1B496 0%, #EA806A 100%)", "#F2CBBD", "#E47958");
     } else {
-        setCorrect(false);
-        updateColours("linear-gradient(180deg, #F6B868 0%, #EE6B2D 100%)", "#F9D29F", "#9F938B");
+      setCorrect(false);
+      updateColours("linear-gradient(180deg, #F6B868 0%, #EE6B2D 100%)", "#F9D29F", "#9F938B");
     }
-  }
+  }, [correctList, selectedList])
 
-  const answerComponent = shuffledAnswers.map((answer, key) => {
+  useEffect(() => {
+    checkCorrectness();
+  }, [selectedList, correctList, correct, checkCorrectness]);
+  
+  const answerComponent = shuffledAnswers.map((answer: string[], key: number) => {
     return (
       <Answers 
       key={key}
       id={key}
       answer={answer}
       correct={correct}
-      handleClick={(e) => handleClick(e)}
+      handleClick={(e: any) => handleClick(e)}
       shuffle={(array) => shuffle(array)}
       />
       )
@@ -76,4 +80,4 @@ const Quiz = () => {
   )
 }
 
-export default Quiz
+export { Quiz };
